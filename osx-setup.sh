e353329e89dafb78cba4f9aee435aed126e94f8f
Nithin Bekal <nithinbekal@gmail.com>
2016-05-31 14:26:57 +0530

    Upgrade nvm from 0.24.0 to 0.31.1

--- osx-setup.sh
+++ osx-setup.sh
@@ -38,7 +38,7 @@ vim +PluginInstall +qall
 mkdir -p ~/.tmp
 
 # Setup node.js
-curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
 nvm install stable
 nvm alias default stable
 
